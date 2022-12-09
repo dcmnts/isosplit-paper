63a49e855962600d67447972eaec6013356bf33c
Jeremy Magland <jmagland@flatironinstitute.org>
2022-12-09 10:44:29 -0500

    udpate

---
+++ test_isocut5.py
@@ -0,0 +1,13 @@
+import numpy as np
+from isocut5 import isocut5
+
+def main():
+    a = 0 + np.random.randn(1000)
+    b = 5 + np.random.randn(1000)
+    c = 10 + np.random.randn(1000)
+    samples = np.concatenate([a, b, c])
+    d, c = isocut5(samples)
+    print(d, c)
+
+if __name__ == '__main__':
+    main()
